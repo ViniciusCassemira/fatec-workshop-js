c5d36a191dd155c4a9e51493b5fce03476ccdfbd
viniciuscassemira <viniciuscruzc2702@gmail.com>
2024-09-05 16:45:09 -0300

    Solução do desafio proposto

---
+++ resolucao.js
@@ -0,0 +1,13 @@
+//Vinicius Cruz Cassemira
+//2º semestre - Desenvolvimento de Software Multiplataforma (DSM)
+
+const y = 5
+let x = 5
+x += 10
+
+function somar () {
+  const r = x + y
+  return r
+}
+
+console.log(somar())
